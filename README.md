6f69384f13fcde5bce37afd015c66a5783a679f0
Pedro Pereira <pedromdspereira.93@gmail.com>
2018-05-12 12:38:21 +0100

    Created asyncData and fetch helpers for a universal data flow. Added CODE_OF_CONDUCT. Added license. Updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # nuxt-neo
-Universal way to take care of data flow, server or client side
+
+> This module allows you to make a middleware API between the browser, your server and other private API's. Opinated, yet flexible, you can take care of your data flow in the same way, no matter if you are executing code on server or client side.
+
+## Work in Progress
+
+### License
+MIT
--- tests/fixtures/pages/index.vue
+++ tests/fixtures/pages/index.vue
@@ -11,17 +11,9 @@
 <script>
     export default {
         name: 'index',
-        asyncData: async function (context) {
-            if (process.server) {
-                return {
-                    data: await context.req.generateControllersTree().users.categories.types.allAction()
-                }
-            }
-
-            return {
-                data: await context.app.$api.users.categories.types.allAction()
-            }
-        },
+        asyncData: asyncData({
+            data: ({app}) => app.$api.users.categories.types.allAction()
+        }),
         methods: {
             async handleClick() {
                 this.data = await this.$api.users.allAction();
