a39d3abc8dfefbabd32b0a41a86486a7b55bf280
Pedro Pereira <pedromdspereira.93@gmail.com>
2019-03-28 18:54:15 +0000

    chore(deploy_docs): make deploy_docs.sh script more configurable

--- deploy_docs.sh
+++ deploy_docs.sh
@@ -3,15 +3,21 @@
 # abort on errors
 set -e
 
+GIT_NAME=$(git config --get user.name)
+GIT_EMAIL=$(git config --get user.email)
+GIT_SSH_COMMAND=$(git config --get core.sshCommand)
+
 # build
-npm run docs:build
+yarn docs:build
 
 # navigate into the build output directory
 cd docs/.vuepress/dist
 
 git init
-git config user.name "Pedro Pereira"
-git config user.email "pedromdspereira.93@gmail.com"
+git config user.name "$GIT_NAME"
+git config user.email "$GIT_EMAIL"
+
+git config core.sshCommand "$GIT_SSH_COMMAND"
 
 git add -A
 git commit -m 'Updated documentation'
