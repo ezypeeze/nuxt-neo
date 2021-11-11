aa5d177f45ede9a5d6fdb2d5fe2bb8e889fd147d
Pedro Pereira <pedromdspereira.93@gmail.com>
2021-11-11 14:16:23 +0000

    ci: release workflows

--- deploy_docs.sh
+++ deploy_docs.sh
@@ -7,6 +7,11 @@ GIT_NAME=$(git config --get user.name)
 GIT_EMAIL=$(git config --get user.email)
 GIT_REMOTE_URL=$(git config --get remote.origin.url)
 
+if [ -z "$1" ] ;  then
+    echo "ERR: first argument must be the version"
+    exit
+fi
+
 # build
 cd docs || exit
 yarn build
@@ -20,7 +25,7 @@ git config user.name "$GIT_NAME"
 git config user.email "$GIT_EMAIL"
 
 git add -A
-git commit -m 'Updated documentation'
+git commit -m "docs: update gh-pages docs: v$1"
 git push -f $GIT_REMOTE_URL master:gh-pages
 
 cd -
