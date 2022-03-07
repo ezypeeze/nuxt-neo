69902e32500a23665dbd63dce346d8937acbd1ab
Rafal Chlodnicki <rchl2k@gmail.com>
2022-03-07 22:44:55 +0100

    test optional cases on both server and client side

--- tests/fixtures/pages/index.vue
+++ tests/fixtures/pages/index.vue
@@ -1,17 +1,19 @@
 <template>
     <div class="index">
-        <span class="path">{{ data.path }}</span>
-        <span class="id-param">{{ data.params && data.params.id }}</span>
-        <span v-if="data.ok" class="okay">It's okay!</span>
-        <span v-else class="okay">It's not okay...</span>
-        <span v-if="data.success_handler" class="response-middleware">It's okay!</span>
-        <span v-else class="response-middleware">It's not okay...</span>
-        <span class="number-of-users">{{ users.length }}</span>
-        <span v-if="users && users[0]" class="first-user">{{ users[0].first_name }}</span>
-        <span v-if="serverForceError" class="server-side-force-error">{{ serverForceError }}</span>
-        <div class="optional-data">{{ optionalData }}</div>
+        <div class="path">{{ data.path }}</div>
+        <div class="id-param">{{ data.params && data.params.id }}</div>
+        <div v-if="data.ok" class="okay">It's okay!</div>
+        <div v-else class="okay">It's not okay...</div>
+        <div v-if="data.success_handler" class="response-middleware">It's okay!</div>
+        <div v-else class="response-middleware">It's not okay...</div>
+        <div class="number-of-users">{{ users.length }}</div>
+        <div v-if="users && users[0]" class="first-user">{{ users[0].first_name }}</div>
+        <div v-if="errorMessage" class="error-message">{{ errorMessage }}</div>
+        <div v-if="errorMessageGetOptional" class="error-message-get-optional">{{ errorMessageGetOptional }}</div>
+        <div v-if="errorMessageGetWithoutParam" class="error-message-get-without-param">{{ errorMessageGetWithoutParam }}</div>
 
-        <button class="change-path" @click="handleClick">Change Path</button>
+        <button class="change-path" @click="handleClickChangePath">Change Path</button>
+        <button class="change-path-without-param" @click="handleClickChangePathWithoutParam">Change Path Without Param</button>
         <button class="create-user" @click="handleCreateUser">Create User</button>
         <button class="get-optional" @click="handleGetOptional">Get Optional</button>
     </div>
@@ -20,45 +22,82 @@
 <script>
 export default {
     name: 'IndexPage',
-    asyncData: async ({ app, route }) => {
+    async asyncData({ $api, route }) {
         if (route.query.nuxtError) {
-            return await app.$api.nuxtError.error();
+            return await $api.nuxtError.error();
         }
 
-        await app.$api.users.createAction({ body: { first_name: 'first', last_name: 'user' } });
-        const { users } = await app.$api.users.allAction();
+        await $api.users.createAction({ body: { first_name: 'first', last_name: 'user' } });
+        const { users } = await $api.users.allAction();
 
-        let serverForceError;
+        let errorMessage;
         try {
-            await app.$api.products.allAction({ query: { force_error: 'true' } });
+            await $api.products.allAction({ query: { force_error: 'true' } });
         } catch (err) {
-            serverForceError = err.message;
+            errorMessage = err.message;
         }
-        await app.$api.users.getActionOptional({ params: { id: '1' } });
+
+        let errorMessageGetOptional;
+        try {
+            await $api.users.getActionOptional({ params: { id: '1' } });
+        } catch (error) {
+            errorMessageGetOptional = error.message;
+        }
+
+        let errorMessageGetWithoutParam;
+        try {
+            await $api.users.getAction();
+        } catch (error) {
+            errorMessageGetWithoutParam = error.message;
+        }
 
         return {
             users,
-            serverForceError,
-            data: await app.$api.users.categories.types.allAction(),
-            optionalData: {}
+            errorMessage,
+            errorMessageGetOptional,
+            errorMessageGetWithoutParam,
+            data: await $api.users.categories.types.allAction()
         };
     },
     methods: {
-        async handleClick() {
-            this.data = await this.$api.users.getAction({ params: { id: '1' } });
+        async handleClickChangePath() {
+            this.errorMessage = undefined;
+            try {
+                this.data = await this.$api.users.getAction({ params: { id: 1 } });
+            } catch (err) {
+                this.errorMessage = err.message;
+            }
+        },
+        async handleClickChangePathWithoutParam() {
+            this.errorMessage = undefined;
+            try {
+                this.data = await this.$api.users.getAction();
+            } catch (err) {
+                this.errorMessage = err.message;
+            }
         },
         async handleCreateUser() {
-            await this.$api.users.createAction({
-                body: {
-                    first_name: 'nuxt',
-                    last_name: 'neo'
-                }
-            });
-            const { users } = await this.$api.users.allAction();
-            this.users = users;
+            this.errorMessage = undefined;
+            try {
+                await this.$api.users.createAction({
+                    body: {
+                        first_name: 'nuxt',
+                        last_name: 'neo'
+                    }
+                });
+                const { users } = await this.$api.users.allAction();
+                this.users = users;
+            } catch (err) {
+                this.errorMessage = err.message;
+            }
         },
         async handleGetOptional() {
-            this.optionalData = await this.$api.users.getActionOptional({ params: { id: '1' } });
+            this.errorMessage = undefined;
+            try {
+                this.data = await this.$api.users.getActionOptional({ params: { id: '1' } });
+            } catch (err) {
+                this.errorMessage = err.message;
+            }
         }
     }
 };
